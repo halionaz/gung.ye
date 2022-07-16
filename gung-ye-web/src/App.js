f28d896965edb611c5db7458ac49c4c44f2e74fe
halionaz <samhsj@naver.com>
2022-07-16 07:52:17 -0400

    ✨ Build a Navi bar

--- gung-ye-web/src/App.js
+++ gung-ye-web/src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
-import Header from './Header';
+import "./style/App.css";
+import Header from "./Header";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-        <Header></Header>
-    </div>
-  );
+    const [curMenu, setCurMenu] = useState("home");
+
+    return (
+        <div className="App">
+            <Header curMenu={curMenu} setCurMenu={setCurMenu} ></Header>
+        </div>
+    );
 }
 
 export default App;
--- gung-ye-web/src/Header.js
+++ gung-ye-web/src/Header.js
@@ -1,10 +1,29 @@
 import logo from "./logo.png";
 import style from "./style/Header.module.css";
 
-export default function Header(){
+export default function Header({curMenu, setCurMenu}){
+
+    const menuChange = () => {
+        setCurMenu((prev) => {
+            if(prev === "home"){
+                return "all";
+            } else {
+                return "home";
+            }
+        })
+    }
+
     return <header className={style.Header}>
         <div className={style.header}>
+            <div className={style.left}>
+                <div className={style.naviBtn} onClick={menuChange} >
+                    <ion-icon name={(curMenu === "home") ? "menu" : "home"}></ion-icon>
+                </div>
+            </div>
+            <div className={`${style.logoDiv}`}>
             <img className={style.logo} src={logo} alt="logo"/>
+            </div>
+            <div className={style.right}></div>
         </div>
     </header>
 }
