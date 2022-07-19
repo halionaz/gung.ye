527c0d5d4337ecc1fe10e0ebb9a2d2e356612526
halionaz <samhsj@naver.com>
2022-07-19 10:09:06 -0400

    🚚 Clean up the file system

--- gung-ye-web/src/App.js
+++ gung-ye-web/src/App.js
@@ -1,7 +1,7 @@
 import "./style/App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Routes/Home";
-import Header from "./Header";
+import Header from "./Components/Header";
 
 function App() {
     return (
