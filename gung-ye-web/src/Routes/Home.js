fc95231ecaa83f666e88ccd91e87542cddfcfbe1
halionaz <samhsj@naver.com>
2022-07-20 09:48:04 -0400

    🚧 Work in Landing Page

--- gung-ye-web/src/Routes/Home.js
+++ gung-ye-web/src/Routes/Home.js
@@ -3,7 +3,7 @@ const Home = () => {
     return (
         <>
             <div style={{ padding: "100px 30px" }}>
-                박정혁은 사람이 될 수 있을까요? <br /> 예 아니오
+                소소한 미래 예측 플랫폼, 궁예
             </div>
         </>
     );
